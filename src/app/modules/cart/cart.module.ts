4fbcfc80a499604762d09d2cfdc3ec616a7c0ac8
Jorge Luis Mayorga <jl.mayorga236@gmail.com>
2023-01-17 17:12:05 -0500

    feat(cart): setup cart routes and components

--- src/app/modules/cart/cart.module.ts
+++ src/app/modules/cart/cart.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartDetailsViewComponent } from './views/cart-details-view/cart-details-view.component';
 import { CartComponent } from './cart.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     CartDetailsViewComponent,
     CartComponent
   ],
-  imports: [CommonModule],
+  imports: [CommonModule , RouterModule.forRoot(AuthRoutes)],
 })
 export class CartModule {}
