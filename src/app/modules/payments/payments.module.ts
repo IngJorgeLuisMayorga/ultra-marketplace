651bccc81b0edc1fd1e4016c19fa3f7697e02606
Jorge Luis Mayorga <jl.mayorga236@gmail.com>
2023-01-17 17:19:07 -0500

    feat(payments): setup payments routes and components

--- src/app/modules/payments/payments.module.ts
+++ src/app/modules/payments/payments.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaymentsComponent } from './payments.component';
+import { Router, RouterModule } from '@angular/router';
+import { PaymentsRoutes } from './payments.routes';
 
 @NgModule({
   declarations: [
     PaymentsComponent
   ],
-  imports: [CommonModule],
+  imports: [CommonModule,  RouterModule.forRoot(PaymentsRoutes)],
+  exports: [RouterModule]
 })
 export class PaymentsModule {}
