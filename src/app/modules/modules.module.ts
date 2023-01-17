7e219ee0b149947dbed3709670d3bcdcd071abbb
Jorge Luis Mayorga <jl.mayorga236@gmail.com>
2023-01-17 17:28:16 -0500

    feat(router): Imports Modules

--- src/app/modules/modules.module.ts
+++ src/app/modules/modules.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthModule } from './auth/auth.module';
+import { CartModule } from './cart/cart.module';
+import { CheckoutModule } from './checkout/checkout.module';
+import { PaymentsModule } from './payments/payments.module';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+
+const COMPONENTS: any[] = [];
+
+const MODULES = [
+  AuthModule,
+  CartModule,
+  CheckoutModule,
+  PaymentsModule,
+  ProductsModule,
+  UsersModule
+]
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [...COMPONENTS],
+  imports: [CommonModule, ...MODULES],
+  exports: [...COMPONENTS],
 })
 export class ModulesModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+const COMPONENTS: any[] = [];
+const MODULES: any[] = [RouterModule, FormsModule]
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [...COMPONENTS],
+  imports: [CommonModule, ...MODULES],
+  exports: [...COMPONENTS],
 })
 export class SharedModule {}
